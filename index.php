a2d87bf7376e76db029b23f58c12eebff47ed7e4
Spencer Piontkowski <spencer.piontkowski@toastedlime.com>
2014-02-07 22:56:56 -0800

    Incorporate WordPress Importer to actually upload a file.

--- index.php
+++ index.php
@@ -92,18 +92,18 @@ function image_importer_uploader(){
 
 	$parameters = array(
 		'url' => $_POST['url'],
-		'title' => $_POST['title'],
+		'post_title' => $_POST['title'],
 		'link' => $_POST['link'],
 		'pubDate' => $_POST['pubDate'],
-		'creator' => $_POST['creator'],
+		'post_author' => $_POST['creator'],
 		'guid' => $_POST['guid'],
-		'post_id' => $_POST['post_id'],
+		'import_id' => $_POST['post_id'],
 		'post_date' => $_POST['post_date'],
 		'post_date_gmt' => $_POST['post_date_gmt'],
 		'comment_status' => $_POST['comment_status'],
 		'ping_status' => $_POST['ping_status'],
 		'post_name' => $_POST['post_name'],
-		'status' => $_POST['status'],
+		'post_status' => $_POST['status'],
 		'post_parent' => $_POST['post_parent'],
 		'menu_order' => $_POST['menu_order'],
 		'post_type' => $_POST['post_type'],
@@ -111,11 +111,90 @@ function image_importer_uploader(){
 		'is_sticky' => $_POST['is_sticky']
 	);
 
-	function upload( $image ){
+	function process_attachment( $post, $url ) {
 		
-		return wp_get_http( $image['url'] );
+		// if the URL is absolute, but does not contain address, then upload it assuming base_site_url
+		if ( preg_match( '|^/[\w\W]+$|', $url ) )
+			$url = rtrim( $this->base_url, '/' ) . $url;
+
+		$upload = fetch_remote_file( $url, $post );
+		if ( is_wp_error( $upload ) )
+			return $upload;
+
+		if ( $info = wp_check_filetype( $upload['file'] ) )
+			$post['post_mime_type'] = $info['type'];
+		else
+			return new WP_Error( 'attachment_processing_error', __('Invalid file type', 'wordpress-importer') );
+
+		$post['guid'] = $upload['url'];
+
+		// as per wp-admin/includes/upload.php
+		$post_id = wp_insert_attachment( $post, $upload['file'] );
+		wp_update_attachment_metadata( $post_id, wp_generate_attachment_metadata( $post_id, $upload['file'] ) );
+
+		// remap resized image URLs, works by stripping the extension and remapping the URL stub.
+		if ( preg_match( '!^image/!', $info['type'] ) ) {
+			$parts = pathinfo( $url );
+			$name = basename( $parts['basename'], ".{$parts['extension']}" ); // PATHINFO_FILENAME in PHP 5.2
+
+			$parts_new = pathinfo( $upload['url'] );
+			$name_new = basename( $parts_new['basename'], ".{$parts_new['extension']}" );
+		}
+
+		return $post_id;
+	}
+	
+	function fetch_remote_file( $url, $post ) {
+		// extract the file name and extension from the url
+		$file_name = basename( $url );
+
+		// get placeholder file in the upload dir with a unique, sanitized filename
+		$upload = wp_upload_bits( $file_name, 0, '', $post['post_date'] );
+		if ( $upload['error'] )
+			return new WP_Error( 'upload_dir_error', $upload['error'] );
+
+		// fetch the remote url and write it to the placeholder file
+		$headers = wp_get_http( $url, $upload['file'] );
+
+		// request failed
+		if ( ! $headers ) {
+			@unlink( $upload['file'] );
+			return new WP_Error( 'import_file_error', __('Remote server did not respond', 'wordpress-importer') );
+		}
+
+		// make sure the fetch was successful
+		if ( $headers['response'] != '200' ) {
+			@unlink( $upload['file'] );
+			return new WP_Error( 'import_file_error', sprintf( __('Remote server returned error response %1$d %2$s', 'wordpress-importer'), esc_html($headers['response']), get_status_header_desc($headers['response']) ) );
+		}
+
+		$filesize = filesize( $upload['file'] );
+
+		if ( isset( $headers['content-length'] ) && $filesize != $headers['content-length'] ) {
+			@unlink( $upload['file'] );
+			return new WP_Error( 'import_file_error', __('Remote file is incorrect size', 'wordpress-importer') );
+		}
+
+		if ( 0 == $filesize ) {
+			@unlink( $upload['file'] );
+			return new WP_Error( 'import_file_error', __('Zero size file downloaded', 'wordpress-importer') );
+		}
+
+		/*
+		 * Allow uploads of any size for now.
+		 
+		$max_size = (int) $this->max_attachment_size();
+		if ( ! empty( $max_size ) && $filesize > $max_size ) {
+			@unlink( $upload['file'] );
+			return new WP_Error( 'import_file_error', sprintf(__('Remote file is too large, limit is %s', 'wordpress-importer'), size_format($max_size) ) );
+		}
+		*/
+
+		return $upload;
 	}
 
-	echo json_encode( upload( $parameters ) );
+	$remote_url = ! empty($parameters['attachment_url']) ? $parameters['attachment_url'] : $parameters['guid'];
+	
+	echo json_encode( process_attachment( $parameters, $remote_url ) );
 
 die();}
